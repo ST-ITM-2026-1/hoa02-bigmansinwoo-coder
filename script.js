3e8bea84345931829cf402a4bc1d636016a4a632
bigmansinwoo <tlsdn5397@naver.com>
2026-04-18 17:38:54 +0900

    implement Project Filtering

--- script.js
+++ script.js
@@ -10,3 +10,27 @@ toggle_button.addEventListener("click", function(){
     }
     }
 )
+
+const filterButton = document.querySelectorAll(".filter-btn");
+const projectCards = document.querySelectorAll(".project-card");
+
+filterButton.forEach(function(button){
+    button.addEventListener("click", function(){
+        const category = button.getAttribute("data-category");
+
+        //update active button
+        filterButton.forEach(function(btn){
+            btn.classList.remove("active");
+        })
+        button.classList.add("active");
+
+        //show or hide projects
+        projectCards.forEach(function(card){
+            if(category === "all" || card.getAttribute("data-category") === category){
+                card.style.display = "block"
+            }else{
+                card.style.display = "none";
+            }
+        })
+    })
+})
